7a315aea593bd04254c041990606ea78e5b91eff
mrcraig <1002386c@student.gla.ac.uk>
2013-11-18 23:40:57 +0000

    IO and openACC working

---
+++ F95OpenACCParser.hs
@@ -0,0 +1,38 @@
+module F95OpenACCParser (
+    extract_OpenACC_regions_from_F95_src
+) where
+import Text.Regex.TDFA -- .Posix -- suggest use of regular expressions
+
+-- given the source code as a list of lines (strings), extract the OpenACC regions for Arguments and ConstArguments as well as the parameter declarations, and return them as a tuple of three lists of strings, in that order.
+extract_OpenACC_regions_from_F95_src :: [String] -> ([String],[String],[String])
+extract_OpenACC_regions_from_F95_src in_src_lines = (extract_args in_src_lines,extract_constargs in_src_lines,extract_params in_src_lines)
+
+extract_args :: [String] -> [String]
+extract_args [] = []
+extract_args (x:xs)
+	| x =~ "$!acc arguments" :: Bool = extract_args_in xs
+	| otherwise = extract_args xs
+
+extract_args_in :: [String] -> [String]
+extract_args_in [] = error "ACC Arguments not closed"
+extract_args_in (x:xs)
+	| x =~ "$!acc end arguments" :: Bool = extract_args xs
+	| otherwise = [x] ++ extract_args_in xs
+
+extract_constargs :: [String] -> [String]
+extract_constargs [] = []
+extract_constargs (x:xs)
+	| x =~ "!$acc constarguments" :: Bool = extract_constargs_in xs
+	| otherwise = extract_constargs xs
+
+extract_constargs_in :: [String] -> [String]
+extract_constargs_in [] = error "ACC ConstArguments not closed"
+extract_constargs_in (x:xs)
+	| x =~ "!$acc end constarguments" :: Bool = extract_constargs xs
+	| otherwise = [x] ++ extract_constargs_in xs
+
+extract_params :: [String] -> [String]
+extract_params [] = []
+extract_params (x:xs)
+	| x =~ "parameter" :: Bool = [x] ++ extract_params xs
+	| otherwise extract_params xs
---
+++ F95SrcIO.hs
@@ -0,0 +1,19 @@
+module F95SrcIO (
+		read_F95_src,
+        write_F95_src
+) where
+import Data.Char (toLower)
+-- Fortran is case-insensitive so turn everything into lowercase
+lc = map toLower
+-- given the name of the file, read it into a list of strings, one per line of source code
+read_F95_src :: String -> IO [String]
+read_F95_src src_name = do
+	contents = readFile src_name
+	let output = lines (lc contents)
+	return output
+
+-- given a list of strings, one per line of source code, and the name of the file, write the strings to the file
+write_F95_src :: String -> [String] -> IO ()
+write_F95_src src_name src_lines =  writeFile src_name (unlines src_lines)
+
+    
